c94792b2556f93fcf4b10ee393bf3398da6a6111
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-04-30 16:57:16 +0900

    貪欲なワーカーと行儀のいいワーカーの仕事に関して再度検証

--- No.1/Resource/main.go
+++ No.1/Resource/main.go
@@ -10,7 +10,6 @@ func main() {
 	var wg sync.WaitGroup
 	var sharedLock sync.Mutex
 	const runtime = 1 * time.Second
-
 	greedyWorker := func() {
 		defer wg.Done()
 
@@ -28,18 +27,33 @@ func main() {
 		defer wg.Done()
 		var count int
 		for begin := time.Now(); time.Since(begin) <= runtime; {
-			for i := 0; i < 3; i++ {
-				sharedLock.Lock()
-				time.Sleep(1 * time.Nanosecond)
-				sharedLock.Unlock()
-			}
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
+			sharedLock.Lock()
+			time.Sleep(1 * time.Nanosecond)
+			sharedLock.Unlock()
+
 			count++
 		}
 		fmt.Printf("Polite worker was able to execute %v work loops\n", count)
 	}
+	fmt.Println("greedyWorkerの計測")
+	wg.Add(1)
+	greedyWorker()
+	wg.Wait()
+	fmt.Println("politeWorkerの計測")
+	wg.Add(1)
+	politeWorker()
+	wg.Wait()
+	fmt.Println("同時計測")
 	wg.Add(2)
 	go greedyWorker()
 	go politeWorker()
-
 	wg.Wait()
 }
