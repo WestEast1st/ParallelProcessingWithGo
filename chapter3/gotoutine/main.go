b9ff94df291e9a2840ef12f3147782ffbd611b7a
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-05-01 11:04:47 +0900

    メモリのアドレス空間の確認用サンプル

--- chapter3/gotoutine/main.go
+++ chapter3/gotoutine/main.go
@@ -18,7 +18,7 @@ go function()
 */
 
 func main() {
-	sample2()
+	sample3()
 }
 
 func sample1() {
@@ -44,4 +44,41 @@ func sample2() {
 	go sayHello()
 	wg.Wait() //終了の待機
 	fmt.Println("終了")
+}
+
+//go routineのアドレス空間の確認
+func sample3() {
+	/*
+	  go routineを利用したクロージャでのアドレス空間が同一であることを確認するためのサンプル
+	*/
+	var wg sync.WaitGroup
+	fmt.Println("アドレス空間が変更される場合")
+	fmt.Println("START")
+	for _, salutation := range []string{"hello", "greetings", "good bay"} {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}()
+	}
+	wg.Wait()
+	/*
+	  good bay
+	  good bay
+	  good bay
+	  と表示される
+	  go routineが開始する前にループが終了するのでsalutationが書き換わってしまう。
+	*/
+	fmt.Println("END")
+	fmt.Println("クロージャに変数として渡しアドレス空間を別に用意する方法")
+	fmt.Println("START")
+	for _, salutation := range []string{"hello", "greetings", "good bay"} {
+		wg.Add(1)
+		go func(salutation string) {
+			defer wg.Done()
+			fmt.Println(salutation)
+		}(salutation)
+	}
+	wg.Wait()
+	fmt.Println("END")
 }
